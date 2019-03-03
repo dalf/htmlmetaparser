5cf930f85f7f03c96b5e50e3a51d09be20ce4bee
Dalf <alex@al-f.net>
2019-03-03 13:30:03 +0100

    First proto : Fetch 1000 URLs and store result into ElasticSearch

--- storage.js
+++ storage.js
@@ -1,20 +1,48 @@
 'use strict';
 
 const elasticsearch = require('elasticsearch');
+const crypto = require('crypto');
+
 const client = new elasticsearch.Client({
   host: 'localhost:9200',
-  log: 'trace'
+  log: 'debug'
 });
 
-function store(metadata) {
-  // Nothing for now
+
+function urlToId(url) {
+  const hash = crypto.createHash('sha256');
+  hash.update(url);
+  return hash.digest('hex');
 }
 
-function loadByUrl(url) {
+function init() {
+  client.indices.create({
+    index: 'url'
+  }, function(err, resp, status) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("create", resp);
+    }
+  });
+}
+
+function storeUrl(data) {
+  client.index({
+    index: 'url',
+    id: urlToId(data['url']),
+    type: 'url',
+    body: data
+  }, function(err, resp, status) {
+    console.log(resp);
+  });
+}
+
+function loadUrl(url) {
   //
 }
 
 module.exports = {
-  store,
-  loadByUrl
+  storeUrl,
+  loadUrl
 }
